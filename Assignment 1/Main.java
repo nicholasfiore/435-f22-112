45615179d4e87fb4c071dab6a176eab3416c269c
nerfcrazy5 <nicholas.fiore1@marist.edu>
2022-09-10 15:41:17 -0400

    Testing file reading
    Tested file reading functionality. Successful.

--- Assignment 1/Main.java
+++ Assignment 1/Main.java
@@ -1,42 +1,21 @@
+//Utility imports
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Node test1 = new Node();
-        //testing default values for an empty constructor
-        System.out.println(test1.getMyItem());
-        System.out.println(test1.getMyNext());
+        File file = new File("./Assignment 1/magicitems.txt");
+        try {
+            Scanner sc = new Scanner(file);
 
-        test1.setMyItem("Bag of Holding");
-        
-        System.out.println(test1.getMyItem());
-
-        Node test2 = new Node("Bag of Devouring");
-        //testing the partial constructor functionality
-        System.out.println(test2.getMyItem());
-        System.out.println(test2.getMyNext());
-
-        test2.setMyNext(test1);
-
-        System.out.println(test2.getMyNext());
-
-        Node test3 = new Node("Handy Haversack", test2);
-        //testing full constructor functionality
-        System.out.println(test3.getMyItem());
-        System.out.println(test3.getMyNext());
-
-        System.out.println();
-        //Testing linked list functionality by iterating through the list
-        Node currentNode = test3;
-        boolean flag = true;
-        while (flag) {
-            System.out.println(currentNode.getMyItem());
-            System.out.println(currentNode.getMyNext());
-            System.out.println();
-            if (currentNode.getMyNext() == null) {
-                flag = false;
-            } else {
-                currentNode = currentNode.getMyNext();
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
             }
-
+        } catch(FileNotFoundException ex) {
+			System.out.println("Failed to find file: " + file.getAbsolutePath());
+        } catch(Exception ex) {
+            System.out.println("Something went wrong.");
+            System.out.println(ex.getMessage());
         }
     }
 }
