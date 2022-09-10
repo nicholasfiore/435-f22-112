970f9fbb510da2cec68ab371514ab8e47c127bba
nerfcrazy5 <nicholas.fiore1@marist.edu>
2022-09-10 14:17:42 -0400

    Testing of a single Node
    Tested functionality of a single Node object

--- Assignment 1/Main.java
+++ Assignment 1/Main.java
@@ -1,3 +1,12 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        Node test = new Node();
+        //testing default values for an empty constructor
+        System.out.println(test.getMyItem());
+        System.out.println(test.getMyNext());
+
+        test.setMyItem("Bag of Holding");
+        
+        System.out.println(test.getMyItem());
+    }
 }
