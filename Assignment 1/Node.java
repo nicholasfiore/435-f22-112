1fb7e3cc821268c19ca5f3b1df1ac905851443a8
nerfcrazy5 <nicholas.fiore1@marist.edu>
2022-09-10 12:25:39 -0400

    Node class set up
    Set up the Node class enough to function (also included more security with accessors and mutators). Might have to change. Might have to change this around (as I'm now seeing that I might need to use characters, not Strings, since the actual magic items will be put into an array I think)

--- Assignment 1/Node.java
+++ Assignment 1/Node.java
@@ -2,11 +2,18 @@
  * The Node class is used to created linked lists of objects in order to more variably 
  */
 public class Node {
-    String myItem = "";
-    Node myNext = null;
+    /* Data Fields */
+    private String myItem = "";
+    private Node myNext = null;
 
+    /* Constructors */
     //No-arg (default) constructor for creating a default Node object
     public Node() {}
+
+    //Partial constructor for just initializing the Item within the Node
+    public Node(String item) {
+        myItem = item;
+    }
 
     //Full constuctor for creating a Node object and assigning an item name and next Node object pointer
     public Node(String item, Node next) {
@@ -14,6 +21,27 @@ public class Node {
         myNext = next;
     }
 
+    /* Accessors and Mutators */
+    //Returns the item String of the Node object
+    public String getMyItem() {
+        return myItem;
+    }
 
+    //Returns the pointer for the next Node object in the list
+    public Node getMyNext() {
+        return myNext;
+    }
+
+    //Changes the value of the item String of a Node object to a new String
+    public void setMyItem(String myItem) {
+        this.myItem = myItem;
+    }
+
+    //Changes the pointer of the next pointer of a Node object to a new pointer to a node Object (or NULL)
+    public void setMyNext(Node myNext) {
+        this.myNext = myNext;
+    }
+
+    /* Functions */
 
 }
