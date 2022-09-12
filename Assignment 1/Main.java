4aee70def781bb03fc4c76b075fa5ee7fb7c052a
nerfcrazy5 <nicholas.fiore1@marist.edu>
2022-09-12 16:15:19 -0400

    Array initialization
    Worked on automatic reading of the .txt and filling an array with each line as a String. Also began work with changing the Node class to handle characters instead of Strings. Included some test print statements to ensure that the array was initialized properly with the correct values.

--- Assignment 1/Main.java
+++ Assignment 1/Main.java
@@ -1,47 +1,54 @@
 //Utility imports
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("./Assignment 1/magicitems.txt");
-        try {
-            Scanner sc = new Scanner(file);
+        /* creates a Scanner object for the input stream (keyboard), prompts the user for the filename/path and stores that into a variable, and then closes the scanner */
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter the filename or path: ");
+        String fileName = keyboard.nextLine();
+        keyboard.close();
 
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+        //creates a new file object for the magicitems.txt file or other .txt file
+        File file = new File(fileName);
+        //long variable to store the total lines of a .txt
+        long totLines;
+        //array to hold all the
+        String[] itemList;
+        /* This try-catch block is for reading in a .txt file, putting each line onto an array, and throwing exceptions if there are any. */
+        try {
+            //Scanner object for scanning the file
+            Scanner fileScanner = new Scanner(file);
+            Path path = Paths.get(fileName);
+            
+            //grabs the amount of lines within the .txt file and prints it
+            totLines = Files.lines(path).count();
+            System.out.println("Total lines: " + totLines);
+
+            itemList = new String[(int)totLines];
+
+            for (int i = 0; i < totLines; i ++) {
+                itemList[i] = fileScanner.nextLine();
             }
+
+            System.out.println(itemList.length);
+            System.out.println(itemList[0]);
+            System.out.println(itemList[665]);
+            
             //closes the scanner after use to save resources
-            sc.close();
+            fileScanner.close();
         } catch(FileNotFoundException ex) {
 			System.out.println("Failed to find file: " + file.getAbsolutePath());
         } catch(Exception ex) {
             System.out.println("Something went wrong.");
             System.out.println(ex.getMessage());
         }
-        //testing the Stack class
-        System.out.println();
-        Node item1 = new Node("Bag of Holding");
-        Node item2 = new Node("Bag of Devouring");
-        Node item3 = new Node("Handy Haversack");
 
-        Stack myStack = new Stack();
 
-        myStack.push(item1);
-        myStack.push(item2);
-        myStack.push(item3);
-
-        System.out.println(myStack.isEmpty());
-
-        Node temp = myStack.getMyHead();
-        while (temp != null) {
-            System.out.println(myStack.pop().getMyItem());
-            temp = temp.getMyNext();
-        }
-
-        System.out.println(myStack.getMyHead());
-
-        System.out.println(myStack.isEmpty());
 
     }
 }
--- Assignment 1/Node.java
+++ Assignment 1/Node.java
@@ -1,9 +1,9 @@
 /*
- * The Node class is used to created linked lists of objects in order to more variably 
+ * The Node class is used to created linked lists of objects in order to more variably control the size of said list without being locked into an array
  */
 public class Node {
     /* Data Fields */
-    private String myItem = "";
+    private char myChar = ' ';
     private Node myNext = null;
 
     /* Constructors */
@@ -11,20 +11,20 @@ public class Node {
     public Node() {}
 
     //Partial constructor for initializing only the Item within the Node
-    public Node(String item) {
-        myItem = item;
+    public Node(char character) {
+        myChar = character;
     }
 
-    //Full constuctor for creating a Node object and assigning an item name and next Node object pointer
-    public Node(String item, Node next) {
-        myItem = item;
+    //Full constuctor for creating a Node object and assigning an character name and next Node object pointer
+    public Node(char character, Node next) {
+        myChar = character;
         myNext = next;
     }
 
     /* Accessors and Mutators */
-    //Returns the item String of the Node object
-    public String getMyItem() {
-        return myItem;
+    //Returns the character String of the Node object
+    public char getMyItem() {
+        return myChar;
     }
 
     //Returns the pointer for the next Node object in the list
@@ -32,16 +32,14 @@ public class Node {
         return myNext;
     }
 
-    //Changes the value of the item String of a Node object to a new String
-    public void setMyItem(String myItem) {
-        this.myItem = myItem;
+    //Changes the value of the character String of a Node object to a new String
+    public void setMyItem(char myChar) {
+        this.myChar = myChar;
     }
 
     //Changes the pointer of the next pointer of a Node object to a new pointer to a node Object (or NULL)
     public void setMyNext(Node myNext) {
         this.myNext = myNext;
     }
-
-    /* Functions */
 
 }
