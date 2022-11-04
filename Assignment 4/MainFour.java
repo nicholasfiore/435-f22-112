944325786e17815f1076292bca7701f08a37c198
nicholasfiore <89951180+nicholasfiore@users.noreply.github.com>
2022-11-04 16:59:58 -0400

    Main restructuring
    Was starting to get messy, so I moved things around, such as reusing some code by making things into methods.

--- Assignment 4/MainFour.java
+++ Assignment 4/MainFour.java
@@ -11,57 +11,51 @@ import java.nio.file.Paths;
 
 public class MainFour {
     public static void main(String[] args) throws IOException {
-        //creates a new file object for the directory holding the text files, and then a list for holding the files
-        File dir = new File("./txt-files/");
-        File[] files = dir.listFiles();
         //long variable to store the total lines of a .txt, then ints to store them per file
-        long totLines;
         int magicLines; //lines in magicitems.txt
         int treeLines; //lines in magicitems-find-in-bst.txt
         int graphLines; //lines in graphs1.txt
 
         //array to hold all the items in the magic items list
-        String[] itemList;
-        
+        String[] itemList = null;
 
-        for (File file : files) {
-            if (file.isFile()) {
-                BufferedReader input = null;
-                String line;
-                try {
-                //gets the path of the current file in order to get the # of lines
-                Path path = Paths.get(file.getName());
-                
-                input = new BufferedReader(new FileReader(file));
-                //grabs the amount of lines within the .txt file and prints it
-                totLines = Files.lines(path).count();
-                System.out.println("Total lines: " + totLines + "\n");
+        magicLines = fileToArray("magicitems.txt", itemList);
+    }
 
-                if (file.getName() == "magicitems.txt") {
-                    //initializes the size of the array with the total lines in the magicitems.txt
-                    itemList = new String[(int)totLines];
+    //takes a file name and a list and puts each line of the file into a String in an array
+    public static int fileToArray(String fileName, String[] list) throws IOException {
+        long totalLines = 0;
+        File file = new File(fileName);
+        BufferedReader input = null;
+        String line;
+        try {
+            //gets the path of the current file in order to get the # of lines
+            Path path = Paths.get(file.getName());
 
-                    //initizializes the array 
-                    for (int i = 0; i < totLines; i ++) {
-                        itemList[i] = input.readLine();
-                    }
-                }
+            input = new BufferedReader(new FileReader(fileName)); 
 
-            
-                } catch(FileNotFoundException ex) {
-                    System.out.println("Failed to find file: " + file.getAbsolutePath());
-                } catch(IOException ex) {
-                    System.out.println(ex);
-                } catch(Exception ex) {
-                    System.out.println("Something went wrong.");
-                    System.out.println(ex.getMessage());
-                    ex.printStackTrace();
-                } finally {
-                    if (input != null) {
-                        input.close();
-                    }
-                }
+            totalLines = Files.lines(path).count();
+
+            list = new String[(int)totalLines];
+
+            for(int i = 0; i < totalLines; i++) {
+                list[i] = input.readLine();
+            }
+
+        } catch(FileNotFoundException ex) {
+            System.out.println("Failed to find file: " + file.getAbsolutePath());
+        } catch(IOException ex) {
+            System.out.println(ex);
+        } catch(Exception ex) {
+            System.out.println("Something went wrong.");
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                input.close();
             }
         }
+
+        return (int)totalLines;
     }
 }
