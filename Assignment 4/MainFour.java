6d85b6d85b978e4993df6361fec9b16de9bd2bee
nicholasfiore <89951180+nicholasfiore@users.noreply.github.com>
2022-11-04 12:05:10 -0400

    scanner to buffered reader
    transitioned from using a scanner to using buffered readers to read the files. Also, the try-catch block is now within a loop to allow the program to read through all of the files

--- Assignment 4/MainFour.java
+++ Assignment 4/MainFour.java
@@ -3,62 +3,65 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 /* 
  * The purpose of this program is TODO
  */
 
 public class MainFour {
-    public static void main(String[] args) {
-        //creates a new file object for the magicitems.txt file
-        String fileName = "magicitems.txt";
-        File file = new File(fileName);
-        //long variable to store the total lines of a .txt
+    public static void main(String[] args) throws IOException {
+        //creates a new file object for the directory holding the text files, and then a list for holding the files
+        File dir = new File("./txt-files/");
+        File[] files = dir.listFiles();
+        //long variable to store the total lines of a .txt, then ints to store them per file
         long totLines;
-        //array to hold all the items in the list
+        int magicLines; //lines in magicitems.txt
+        int treeLines; //lines in magicitems-find-in-bst.txt
+        int graphLines; //lines in graphs1.txt
+
+        //array to hold all the items in the magic items list
         String[] itemList;
-
         
-        /* This try-catch block is for reading in a .txt file, putting each line onto an array, and throwing exceptions if there are any. */
-        try {
-            //Scanner object for scanning the file
-            Scanner fileScanner = new Scanner(file);
-            Path path = Paths.get(fileName);
-            
-            //grabs the amount of lines within the .txt file and prints it
-            totLines = Files.lines(path).count();
-            System.out.println("Total lines: " + totLines + "\n");
 
-            //initializes the size of the array with the total lines in the .txt
-            itemList = new String[(int)totLines];
+        for (File file : files) {
+            if (file.isFile()) {
+                BufferedReader input = null;
+                String line;
+                try {
+                //gets the path of the current file in order to get the # of lines
+                Path path = Paths.get(file.getName());
+                
+                input = new BufferedReader(new FileReader(file));
+                //grabs the amount of lines within the .txt file and prints it
+                totLines = Files.lines(path).count();
+                System.out.println("Total lines: " + totLines + "\n");
+
+                if (file.getName() == "magicitems.txt") {
+                    //initializes the size of the array with the total lines in the magicitems.txt
+                    itemList = new String[(int)totLines];
+
+                    //initizializes the array 
+                    for (int i = 0; i < totLines; i ++) {
+                        itemList[i] = input.readLine();
+                    }
+                }
 
             
-            for (int i = 0; i < totLines; i ++) {
-                itemList[i] = fileScanner.nextLine();
+                } catch(FileNotFoundException ex) {
+                    System.out.println("Failed to find file: " + file.getAbsolutePath());
+                } catch(IOException ex) {
+                    System.out.println(ex);
+                } catch(Exception ex) {
+                    System.out.println("Something went wrong.");
+                    System.out.println(ex.getMessage());
+                    ex.printStackTrace();
+                } finally {
+                    if (input != null) {
+                        input.close();
+                    }
+                }
             }
-            
-            //closes the scanner after use to save resources
-            fileScanner.close();
-
-
-            /* 
-             * 
-             */
-            //space for graphs
-            
-
-            //Binary Search Trees
-
-
-            
-            
-        } catch(FileNotFoundException ex) {
-			System.out.println("Failed to find file: " + file.getAbsolutePath());
-        } catch(Exception ex) {
-            System.out.println("Something went wrong.");
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
         }
     }
 }
